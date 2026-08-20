50979579fdb6b7947dfb0f6d22a53d16d7ab843e
Mirza <mmhalilovi1@etf.unsa.ba>
2026-08-20 10:38:57 +0200

    Project structure

---
+++ Infrastructure/Services/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class ProductService : IProductService
+    {
+        public string GetProductName()
+        {
+            return "Laptop";
+        }
+    }
+}
