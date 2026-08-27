ec7bc57f4469ad5f4ce6a1b003f3efe581265c76
Mirza <mmhalilovi1@etf.unsa.ba>
2026-08-27 12:39:34 +0200

    Clean files

--- Api/Program.cs
+++ Api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<AppDbContext>());
 builder.Services.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 var app = builder.Build();
 
---
+++ Infrastructure/Security/JwtTokenGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Security
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration; // čita konfiguracijske postavke iz npr. appsettings.json
+
+        public JwtTokenGenerator(IConfiguration configuration) 
+        { 
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),   // ID korisnika kojem token pripada
+                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(ClaimTypes.Role, user.Role?.ToString() ?? "None"),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // ID samog tokena
+            };
+
+            // ključ koji potpisuje token
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: credentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
